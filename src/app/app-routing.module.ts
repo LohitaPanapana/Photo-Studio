f877dee531ee77a8e9a2a56a279b5bda5fab34f3
LohitaPanapana <lohita59@yahoo.com>
2021-02-10 16:36:50 +0530

    Added routing and style related changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'search', pathMatch:'full',
+  },
+  {
+    path: 'search', loadChildren: () => import('./search/search.module').then(m => m.SearchModule)
+  },
+  {
+    path: 'random', loadChildren: () => import('./random/random.module').then(m => m.RandomModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
