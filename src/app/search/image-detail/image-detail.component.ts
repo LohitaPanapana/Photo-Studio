22c15766bbd07690260283ab9b7340fb6b9a93dd
LohitaPanapana <lohita59@yahoo.com>
2021-02-12 15:26:14 +0530

    Added image detail component

---
+++ src/app/search/image-detail/image-detail.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/Operators';
+import { ImageDetails, PhotoService } from 'src/app/photo.service';
+
+@Component({
+  selector: 'app-image-detail',
+  templateUrl: './image-detail.component.html',
+  styleUrls: ['./image-detail.component.css']
+})
+export class ImageDetailComponent implements OnInit {
+  imageDetails$;
+
+  constructor(private photoService: PhotoService) { }
+
+  ngOnInit(): void {
+    this.imageDetails$ = this.photoService.selectedImage;
+    console.log(this.imageDetails$.getValue())
+  }
+
+}
